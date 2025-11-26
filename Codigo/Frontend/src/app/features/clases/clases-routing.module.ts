e5cdc36761fccb74d56073c63d30138cbfb27f38
agustin <agusdasilva2004@gmail.com>
2025-11-26 12:57:18 -0300

    Agrego detalles front y un poco de clase

--- Codigo/Frontend/src/app/features/clases/clases-routing.module.ts
+++ Codigo/Frontend/src/app/features/clases/clases-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ClasesComponent } from './clases.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: ClasesComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
